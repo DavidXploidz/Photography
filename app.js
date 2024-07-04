8bae357efd0d4773fd9eda0f3c264a038120f8ea
DavidXploidz <david.adame.itd@gmail.com>
2024-07-04 16:49:07 -0600

    animaciones ajustadas y textos

--- app.js
+++ app.js
@@ -7,48 +7,51 @@ document.addEventListener("DOMContentLoaded", (event) => {
         scrollTrigger: {
             trigger: ".main",
             pin: ".main",
-            toggleActions: "play none none complete",
             scrub: 1,
-            markers: true,
+            // markers: true,
         }
     })
 
-    // main.from('.main__gear', {rotation: 360,})
     main.from('.main__welcome', {x: -100, opacity: 0, scale: 0.5})
     main.from('.main button', {x: -100, opacity: 0, scale: 0.5})
     main.to('.main__welcome', {x: 0, opacity: 1})
     main.to('.main button', {x: 0, opacity: 1})
-    // main.to('.main__gear', {rotation: 0,})
+
+    // Gear
+    const gear = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".main",
+            scrub: 1,
+        }
+    })
+
+    gear.from('.main__gear', {rotation: 720,})
 
     // Animacion para la seccion 1
     const section_1 = gsap.timeline({
         scrollTrigger: {
             trigger: ".section-one",
             pin: '.section-one',
-            toggleActions: "play none none complete",
             scrub: 1,
             // markers: true,
         }
     })
 
-    section_1.from('.hero h1',{ x: -500, opacity: 0, scale: 0.5})
-    section_1.from('.hero__image',{ x: 500, opacity: 0, scale: 0.5})
-    section_1.from('.hero p',{ x: -500, opacity: 0, scale: 0.5})
-    section_1.from('.hero button',{ x: -500, opacity: 0, scale: 0.5})
-    section_1.to('.hero h1',{ x: 0, opacity: 1,})
-    section_1.to('.hero__image',{ x: 0, opacity: 1,})
-    section_1.to('.hero p',{ x: 0, opacity: 1,})
-    section_1.to('.hero button',{ x: 0, opacity: 1,})
+    section_1.from('.hero h1',{ x: 500, opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_1.from('.hero__image',{ x: -500, opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_1.from('.hero p',{ x: 500, opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_1.to('.hero h1',{ x: 0, opacity: 1, filter: "blur(0px)",})
+    section_1.to('.hero__image',{ x: 0, opacity: 1, filter: "blur(0px)",})
+    section_1.to('.hero p',{ x: 0, opacity: 1, filter: "blur(0px)",})
 
     // Animacion para la seccion 2
     const section_2 = gsap.timeline({
         scrollTrigger: {
             trigger: '.section-two h1',
-            start: 'top 70%',
+            start: 'top 60%',
             end: 'top 30%',
             scrub: 1,
-            toggleActions: "restart none none none",
-            // markers: true,
+            markers: true,
         }
     })
     section_2.from('.section-two .contenedor',{
