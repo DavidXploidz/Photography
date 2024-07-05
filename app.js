2082a535c5509f29fae4f4399341950c86f35a5c
DavidXploidz <david.adame.itd@gmail.com>
2024-07-05 13:36:14 -0600

    seccion de my job agregada

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
     })
 
     main.from('.main__welcome', {x: -100, opacity: 0, scale: 0.5})
-    main.from('.main button', {x: -100, opacity: 0, scale: 0.5})
     main.to('.main__welcome', {x: 0, opacity: 1})
+    main.from('.main button', {x: -100, opacity: 0, scale: 0.5})
     main.to('.main button', {x: 0, opacity: 1})
 
     // Gear
@@ -38,32 +38,41 @@ document.addEventListener("DOMContentLoaded", (event) => {
     })
 
     section_1.from('.hero h1',{ x: 500, opacity: 0, scale: 0.5, filter: "blur(10px)",})
-    section_1.from('.hero__image',{ x: -500, opacity: 0, scale: 0.5, filter: "blur(10px)",})
-    section_1.from('.hero p',{ x: 500, opacity: 0, scale: 0.5, filter: "blur(10px)",})
     section_1.to('.hero h1',{ x: 0, opacity: 1, filter: "blur(0px)",})
+    section_1.from('.hero__image',{ x: -500, opacity: 0, scale: 0.5, filter: "blur(10px)",})
     section_1.to('.hero__image',{ x: 0, opacity: 1, filter: "blur(0px)",})
+    section_1.from('.hero p',{ x: 500, opacity: 0, scale: 0.5, filter: "blur(10px)",})
     section_1.to('.hero p',{ x: 0, opacity: 1, filter: "blur(0px)",})
 
     // Animacion para la seccion 2
     const section_2 = gsap.timeline({
         scrollTrigger: {
-            trigger: '.section-two h1',
-            start: 'top 60%',
-            end: 'top 30%',
+            trigger: '.section-two',
+            start: 'top 80%',
+            end: 'bottom 70%',
             scrub: 1,
-            markers: true,
+            // markers: true,
         }
     })
-    section_2.from('.section-two .contenedor',{
-        opacity: 0,
-        x: -200,
-        filter: "blur(10px)",
-    })
-    section_2.to('.section-two .contenedor',{
-        opacity: 1,
-        x: 0,
-        filter: "blur(0px)"
-    })
+
+    section_2.from('.section-two h1',{ opacity: 0, filter: "blur(10px)", scale: 0.5})
+    section_2.to('.section-two h1',{ opacity: 1, filter: "blur(0px)",})
+    section_2.from('.collage img:nth-child(1)',{ opacity: 0,  scale: 0.5, filter: "blur(10px)",})
+    section_2.to('.collage img:nth-child(1)',{ opacity: 1, filter: "blur(0px)",})
+    section_2.from('.collage img:nth-child(2)',{ opacity: 0,  scale: 0.5, filter: "blur(10px)",})
+    section_2.to('.collage img:nth-child(2)',{ opacity: 1, filter: "blur(0px)",})
+    section_2.from('.collage img:nth-child(3)',{ opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_2.to('.collage img:nth-child(3)',{ opacity: 1, filter: "blur(0px)",})
+    section_2.from('.collage img:nth-child(4)',{ opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_2.to('.collage img:nth-child(4)',{ opacity: 1, filter: "blur(0px)",})
+    section_2.from('.collage img:nth-child(5)',{ opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_2.to('.collage img:nth-child(5)',{ opacity: 1, filter: "blur(0px)",})
+    section_2.from('.collage img:nth-child(6)',{ opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_2.to('.collage img:nth-child(6)',{ opacity: 1, filter: "blur(0px)",})
+    section_2.from('.collage img:nth-child(7)',{ opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_2.to('.collage img:nth-child(7)',{ opacity: 1, filter: "blur(0px)",})
+    section_2.from('.collage img:nth-child(8)',{ opacity: 0, scale: 0.5, filter: "blur(10px)",})
+    section_2.to('.collage img:nth-child(8)',{ opacity: 1, filter: "blur(0px)",})
 
 
 
